7063dacc03818a674b83cdafa7edada760d0c10a
Oussama <Oussama_gtacj@live.fr>
2018-03-10 21:07:33 +0000

    Add files via upload
    + Information fetch.
    + Error and Results layouts.

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,58 @@
-// Global variables----------------------------------------------------------
-var
-	textBox = document.getElementById('textBox'),
-	resultsPanel = document.getElementById('resultsPanel');
-
-
-// Event handlers------------------------------------------------------------
-
-textBox.addEventListener('input', function() {
-	let input = this.value.trim();
+$(document).ready(function() {
+	var
+		$textBox = $('#textBox'),
+		$resultsPanel = $('#resultsPanel'),
+		$errorPanel = $('#errorPanel'),
+		$errorText = $('#errorText'),
+		$profilePanel = $('#profilePanel');
 	
-	if(input.length > 0)
-	{
-		let user = null;
-		resultsPanel.innerHTML = '';
-		
-		$.ajax({
-			url: 'https://api.github.com/users/' + input,
-			data: {
-				client_id: 'ad6b78c61d1707f68319',
-				client_secret: '23b42ef8f03b8b78288ae50dc70910ecde3b7b73'
-			},
-			error: function() {
-				let div = document.createElement('div');
-				let h3 = document.createElement('h3');
-				h3.innerHTML = `The user ${input} doesn't exist!`;
-				div.appendChild(h3);
-				resultsPanel.appendChild(div);
-			}
-		}).done(function(user) {
-				console.log(user);
-		});
+	$textBox.on('input', function() {
+		let $input = $(this).val().trim();
 
-	}
+		if($input.length > 0)
+		{
+			let user = null;
+			
+			$.ajax({
+				url: 'https://api.github.com/users/' + $input,
+				data: {
+					client_id: 'ad6b78c61d1707f68319',
+					client_secret: '23b42ef8f03b8b78288ae50dc70910ecde3b7b73'
+				},
+				error: function() {
+					$resultsPanel.hide();
+					$errorPanel.show();
+
+					$errorText.html(`<i class="fas fa-exclamation-triangle"></i> The user <span>${$input}</span> does not exist!`);
+				}
+			}).done(function(user) {
+				$errorPanel.hide();
+				$resultsPanel.show();
+				
+				$profilePanel.html(`
+					<div id="profileLeft">
+						<img src="${user.avatar_url}" alt="${user.login}'s avatar"><br>
+						<a href="${user.html_url}" target="_blank"><button>Profile</button></a>
+					</div>
+					<div id="profileRight">
+						<h2>${user.login}</h2>
+						<hr>
+						<ul>
+							<li><span><i class="fas fa-user"></i>  Name:</span> ${user.name}<li>
+							<li><span><i class="fas fa-compass"></i>  Location:</span> ` + user.location + `<li>
+							<li><span><i class="fas fa-calendar-alt"></i>  Joined at:</span> ${user.created_at}<li>
+							<li><span><i class="fas fa-building"></i>  Company:</span> ${user.company}<li>
+							<li><span><i class="fas fa-at"></i>  E-mail:</span> ${user.email}<li>
+							<li><span><i class="fas fa-info-circle"></i>  Bio:</span> ${user.bio}<li>
+						</ul>
+					</div>
+				`);
+			});
+		}
+		
+		else {
+			$resultsPanel.hide()
+			$errorPanel.hide();
+		}
+	});
 });
